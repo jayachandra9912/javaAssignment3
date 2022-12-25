c6b69c5890c41774255eef7a1cd2afe2558b5eb6
jayac <jayac@JAYACHANDRA>
2022-12-25 13:48:42 +0530

    javaAssignment3

---
+++ Assignment3/src/ObjCls.java
@@ -0,0 +1,21 @@
+
+public class ObjCls {
+	int std_id;
+	String std_name;
+	String Address;
+	String Branch;
+	ObjCls(){
+		std_id=0;
+		Address="";
+		Branch = "";
+	}
+	ObjCls(int std_id,String std_name,String Address,String Branch){
+		
+		this.std_id=std_id;
+		this.std_name=std_name;
+		this.Address=Address;
+		this.Branch=Branch;
+		
+	}
+
+}
---
+++ Assignment3/src/driverCls.java
@@ -0,0 +1,16 @@
+
+public class driverCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ObjCls obj;
+		obj = new ObjCls(101,"chandra","warangal","cse");
+		System.out.println("id : "+ obj.std_id);
+		System.out.println("name : "+ obj.std_name);
+		System.out.println("address : "+ obj.Address);
+		System.out.println("branch : "+ obj.Branch);
+		
+
+	}
+
+}
